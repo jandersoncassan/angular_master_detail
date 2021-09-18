f37f2bcccc2017b9478921749cc57dd8c9f4ce95
Janderson Cassan <jande.max@gmail.com>
2021-09-18 16:41:03 -0300

    Improving Entries modules

--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -15,7 +15,7 @@ export class EntryListComponent implements OnInit {
   ngOnInit() {
     this.service.getAll()
     .subscribe(
-      entries => this.entries = entries,
+      entries => this.entries = entries.sort((a,b) => b.id - a.id),
       error => alert('Erro ao carregar a lista de categrias')
       )
   }
